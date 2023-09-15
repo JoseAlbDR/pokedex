79c57b6db1136732b9bf9556de20547c95b3f92b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 18:55:43 +0200

    feat: create service

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+
+    let pokemon = await this.pokemonModel.findOne({
+      no: createPokemonDto.no,
+    });
+
+    if (pokemon)
+      throw new ConflictException(
+        `Pokemon ${createPokemonDto.name} already exists`,
+      );
+
+    pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    console.log(pokemon);
+    return pokemon;
   }
 
   findAll() {
