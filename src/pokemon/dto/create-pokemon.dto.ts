56540be452b85c85dd2d1333de894e9727bf1150
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-15 17:24:45 +0200

    feat: add more validations to CreatePokemonDto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,11 +1,19 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreatePokemonDto {
   @IsNotEmpty()
   @IsNumber()
+  @IsPositive()
   no: number;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
   name: string;
 }
