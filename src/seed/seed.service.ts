efa0e731cf1137e0f9e257ff3ed1156b6b0bae6b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-16 22:18:55 +0200

    feat: seed with pokemonModel

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+// import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor(
+    // @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+    private readonly pokemonService: PokemonService,
+  ) {}
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10&offset=10',
@@ -15,6 +21,7 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const no = +url.split('/').at(-2);
       this.pokemonService.create({ no, name });
+      // this.pokemonModel.create({ no, name });
     });
 
     return `Database populated`;
