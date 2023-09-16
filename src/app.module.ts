dc9a29fd2f2b7eb74e5d5dec9fd11b7a74b0c0bf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-17 01:03:41 +0200

    feat: import ConfigModule.forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
@@ -21,4 +24,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
