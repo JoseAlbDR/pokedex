cd9f775c5dced364b4a54d36f034e86e59cbbb7a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-17 01:08:36 +0200

    feat: use environment variables instead of hardcoding mongo url and port

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_URL),
 
     PokemonModule,
 
